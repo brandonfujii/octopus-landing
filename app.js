e2150c77c97f88e279dbd64c3de17f6ecd59f173
brandonfujii <brandonfujii2018@u.northwestern.edu>
2016-04-11 21:50:05 -0500

    added gitignore, rm untracked files

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-// require dependencies
+// DEPENDENCIES
 var express = require('express'),
 app = express(),
 path = require('path');
 var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
 
+// CONTROLLERS
 var indexController = require('./controllers/index');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Get clientside assets
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(favicon(path.join(__dirname, 'public', ''))); //get favicon
 
@@ -21,7 +23,7 @@ app.use(bodyParser.urlencoded({
 app.get('/', indexController.index);
 
 
-
+// SERVER
 app.listen(process.env.PORT || 1337, function(){
   console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env);
 });
